f77b2a2825e7699f4ff61aa94bfc3eb33b8c81e3
Arian Mostashari <53341622+ArianMost@users.noreply.github.com>
2023-03-10 02:55:12 +0100

    Update Constants.kt

--- app/src/main/java/com/example/vivira_arian_mostashari/utils/Constants.kt
+++ app/src/main/java/com/example/vivira_arian_mostashari/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.example.vivira_arian_mostashari.utils
 
 const val BASE_URL = "https://api.github.com/"
 
-const val TOKEN = "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";
+const val TOKEN = "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";
 
 const val PAGE_SIZE = 30;
 
